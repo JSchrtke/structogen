f48c8e5eb5583e3b1aa8821c10443c9172fba669
JSchrtke <jschrtke@protonmail.com>
2020-11-24 21:01:50 +0100

    Add tokenizing of name

--- tokenizer.go
+++ tokenizer.go
@@ -3,5 +3,21 @@ package main
 type Tokenizer struct{}
 
 func (t *Tokenizer) makeTokens(s string) []Token {
-	return []Token{}
+	var tokens []Token
+	var runes []rune
+	lineNumber := 1
+	for i, r := range s {
+		runes = append(runes, r)
+		switch string(runes) {
+		case "name":
+			nameToken := Token{
+				tokenType: "name",
+				value:     "name",
+				line:      lineNumber,
+				column:    i,
+			}
+			tokens = append(tokens, nameToken)
+		}
+	}
+	return tokens
 }
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -12,3 +12,16 @@ func TestTokenizingEmptyStringDoesNothing(t *testing.T) {
 		t.Errorf(fmt.Sprintf("Expected no tokens, but got %d", len(tokens)))
 	}
 }
+
+func TestCanTokenizeName(t *testing.T) {
+	var tokenizer Tokenizer
+	tokens := tokenizer.makeTokens("name")
+	if len(tokens) != 1 {
+		t.Errorf(fmt.Sprintf("Expected 1 token, but got %d", len(tokens)))
+	}
+	if tokens[0].tokenType != "name" {
+		t.Errorf(fmt.Sprintf(
+			"Expected token of type name, but got %s", tokens[0].tokenType,
+		))
+	}
+}
